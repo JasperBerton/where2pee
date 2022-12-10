a6dadd013a8abd42fd6770ecdd9b10007e0e288f
Jasper Berton <jasper.berton@student.hogent.be>
2022-12-10 14:28:10 +0100

    map added

--- frontend-app/src/components/Navigation.jsx
+++ frontend-app/src/components/Navigation.jsx
@@ -3,20 +3,20 @@ import {Link} from 'react-router-dom'
 export default function Navigation()
 {
   return(
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type='button' data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls='navbarNav' aria-expanded="false" aria-label='Toggle navigation'>
-          <span class="navbar-toggler-icon">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type='button' data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls='navbarNav' aria-expanded="false" aria-label='Toggle navigation'>
+          <span className="navbar-toggler-icon">
           </span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link to="/" class="nav-link">Kaart</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Kaart</Link>
             </li>
-            <li class="nav-item">
-              <Link to="/about" class="nav-link">Over</Link>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">Over</Link>
             </li>
           </ul>
         </div>
--- frontend-app/src/pages/Map.jsx
+++ frontend-app/src/pages/Map.jsx
@@ -1,8 +1,23 @@
 import Navigation from "../components/Navigation"
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { useMap } from 'react-leaflet/hooks'
+import { Marker, Popup } from "react-leaflet"
 
 export default function Map(){
 return <div>
     <Navigation/>
-    <h1>Beeld je in dat je een kaart met wc's ziet</h1>
+    <h1>Toiletten</h1>
+    <MapContainer center={[51.0543422, 3.7174243]} zoom={13} scrollWheelZoom={false} id="map">
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
 </div>
 }
