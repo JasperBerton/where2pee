7814b5e06dc0b96d6f2258dbb7087d3cb90e069e
Jasper Berton <jasper.berton@student.hogent.be>
2023-02-03 21:00:08 +0100

    some new css

--- frontend-app/src/components/Navigation.jsx
+++ frontend-app/src/components/Navigation.jsx
@@ -6,7 +6,7 @@ export default function Navigation()
 {
   const {isAuthenticated} = useAuth0();
   return(
-    <nav className="navbar navbar-expand-lg ">
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">Where2Pee</a>
         <button className="navbar-toggler" type='button' data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls='navbarNav' aria-expanded="false" aria-label='Toggle navigation'>
